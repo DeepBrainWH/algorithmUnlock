545e2395330f0a5470f683e2114e69ebb3f8b037
wangheng <673602165@qq.com>
2018-11-16 15:58:10 +0800

    linux commit

---
+++ graph/GraphKeyRoute.h
@@ -0,0 +1,17 @@
+//
+// Created by wangheng on 11/16/18.
+//
+/***
+ * 关键路径算法
+ */
+#ifndef ALGORITHMUNLOCK_GRAPHKEYROUTE_H
+#define ALGORITHMUNLOCK_GRAPHKEYROUTE_H
+
+typedef int edge;
+
+class GraphKeyRoute {
+
+};
+
+
+#endif //ALGORITHMUNLOCK_GRAPHKEYROUTE_H
